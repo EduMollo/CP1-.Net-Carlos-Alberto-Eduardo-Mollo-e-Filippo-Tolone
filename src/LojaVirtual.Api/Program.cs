6055aac8e508152f3a8927bf72781cdc4756e8c8
carlosguedesneto <dudu.mollo05@gmail.com>
2026-03-27 14:07:12 -0300

    Final

--- src/LojaVirtual.Api/Program.cs
+++ src/LojaVirtual.Api/Program.cs
@@ -1,18 +1,16 @@
+using LojaVirtual.Infrastructure.Persistance;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddDbContext<LojaVirtualContext>(options =>
+    options.UseOracle(builder.Configuration.GetConnectionString("OracleConnection")));
+
+
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
 
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
---
+++ src/LojaVirtual.Infrastructure/Persistance/LojaVirtualContext.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using LojaVirtual.Domain.Entities;
+using LojaVirtual.Domain.Commons;
+
+namespace LojaVirtual.Infrastructure.Persistance
+{
+    public class LojaVirtualContext : DbContext
+    {
+        public LojaVirtualContext(DbContextOptions<LojaVirtualContext> options) 
+            : base(options) 
+        { }
+
+       
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Loja> Lojas { get; set; }
+        public DbSet<Estoque> Estoques { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<EstoqueProduto> EstoqueProdutos { get; set; }
+        public DbSet<CategoriaProduto> CategoriaProdutos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+          
+            modelBuilder.Entity<EstoqueProduto>()
+                .HasKey(ep => new { ep.EstoqueId, ep.ProdutoId });
+                
+            modelBuilder.Entity<CategoriaProduto>()
+                .HasKey(cp => new { cp.CategoriaId, cp.ProdutoId });
+        }
+    }
+}
